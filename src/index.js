c87fd4e32b4e76e393a4a2e5e33eb7e3ec763076
vampiressa <yuliadervenkova@gmail.com>
2019-09-24 13:48:34 +0300

    brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let data = [];
+    if (str.length % 2 !== 0) {
+        return false;
+    }
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+            if (str[i] === bracketsConfig[j][1] && data[data.length - 1] === bracketsConfig[j][0]) {
+                data.pop();
+            } else if (str[i] === bracketsConfig[j][0]) {
+                data.push(bracketsConfig[j][0]);
+            }
+        }
+    }
+    return data.length === 0;
+};
